75926eb468b002f08aa1ed00c605c167d8f264bb
Veronika Deiko <deykonika2005@gmail.com>
2024-02-25 00:07:23 +0200

    шаблон сторінки управління

---
+++ BeautySalonInfrastructure/Views/Home/Adding.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautySalonInfrastructure.Views.Home
+{
+    public class AddingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
