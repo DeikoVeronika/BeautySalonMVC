ca99d660c4aca998712872f3121ea895af51e34c
Veronika Deiko <deykonika2005@gmail.com>
2024-02-26 10:02:31 +0200

    controllers for the reservation creation page

--- BeautySalonInfrastructure/Controllers/ReservationsController.cs
+++ BeautySalonInfrastructure/Controllers/ReservationsController.cs
@@ -50,18 +50,19 @@ namespace BeautySalonInfrastructure.Controllers
         // GET: Reservations/Create
         public IActionResult Create()
         {
-            ViewData["ClientsId"] = new SelectList(_context.Clients, "Id", "LastName");
-            ViewData["SchedulesId"] = new SelectList(_context.Schedules, "Id", "Id");
-            ViewData["ServicesId"] = new SelectList(_context.Services, "Id", "Name");
+            ViewData["Clients.FirstName"] = new SelectList(_context.Clients, "Id", "FirstName");
+            ViewData["Clients.LastName"] = new SelectList(_context.Clients, "Id", "LastName");
+            ViewData["Clients.Phone"] = new SelectList(_context.Clients, "Id", "Phone");
+            ViewData["Schedules.Date"] = new SelectList(_context.Schedules, "Id", "Date");
+            ViewData["Schedules.StartTime"] = new SelectList(_context.Schedules, "Id", "StartTime");
+            ViewData["TypeServices.Name"] = new SelectList(_context.Services, "Id", "Name");
+            ViewData["Service.Name"] = new SelectList(_context.Services, "Id", "Name");
             return View();
         }
 
-        // POST: Reservations/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ClientsId,ServicesId,SchedulesId,Info,Id")] Reservation reservation)
+        public async Task<IActionResult> Create([Bind("Clients.FirstName,Clients.LastName,Clients.Phone,Schedules.Date,Schedules.StartTime,TypeServices.Name,Services.Name,Id")] Reservation reservation)
         {
             if (ModelState.IsValid)
             {
@@ -69,11 +70,17 @@ namespace BeautySalonInfrastructure.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClientsId"] = new SelectList(_context.Clients, "Id", "LastName", reservation.ClientsId);
-            ViewData["SchedulesId"] = new SelectList(_context.Schedules, "Id", "Id", reservation.SchedulesId);
-            ViewData["ServicesId"] = new SelectList(_context.Services, "Id", "Name", reservation.ServicesId);
+            ViewData["Clients.FirstName"] = new SelectList(_context.Clients, "Id", "FirstName", reservation.Clients.FirstName);
+            ViewData["Clients.LastName"] = new SelectList(_context.Clients, "Id", "LastName", reservation.Clients.LastName);
+            ViewData["Clients.Phone"] = new SelectList(_context.Clients, "Id", "Phone", reservation.Clients.Phone);
+            ViewData["Schedules.Date"] = new SelectList(_context.Schedules, "Id", "Date", reservation.Schedules.Date);
+            ViewData["Schedules.StartTime"] = new SelectList(_context.Schedules, "Id", "StartTime", reservation.Schedules.StartTime);
+            ViewData["TypeServices.Name"] = new SelectList(_context.Services, "Id", "Name", reservation.TypeServices.Name);
+            ViewData["Services.Name"] = new SelectList(_context.Services, "Id", "Name", reservation.Services.Name);
             return View(reservation);
         }
+
+
 
         // GET: Reservations/Edit/5
         public async Task<IActionResult> Edit(int? id)
