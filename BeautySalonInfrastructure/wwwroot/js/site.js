499c999012454fb25650664a2c2ae0d1ed0edf6d
Veronika Deiko <deykonika2005@gmail.com>
2024-03-04 21:55:41 +0200

    added the design of the main page

--- BeautySalonInfrastructure/wwwroot/js/site.js
+++ BeautySalonInfrastructure/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿
+﻿let carouselEl = document.getElementById('bootstrap-gallery-carousel');
+
+const carousel = new bootstrap.Carousel(carouselEl, {
+    interval: 3000,
+    wrap: true,
+});
+
+carouselEl.addEventListener('slide.bs.carousel', (event) => {
+    const container = document.querySelector('.carousel-inner');
+    window.lightGallery(container, {
+        thumbnail: true,
+        pager: false,
+        plugins: [lgThumbnail],
+        hash: false,
+        zoomFromOrigin: false,
+        preload: 3,
+        selector: '.lg-item',
+    });
+});
