59974ed2cb371dd439917e2d6f5b67a33dbb1f56
Veronika Deiko <deykonika2005@gmail.com>
2024-03-10 21:20:06 +0200

    added ChartClientReservations

---
+++ BeautySalonInfrastructure/Controllers/ChartClientReservationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautySalonInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartClientReservationsController : ControllerBase
+    {
+        private readonly DbbeautySalonContext _context;
+
+        public ChartClientReservationsController(DbbeautySalonContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var clients = _context.Clients.Include(c => c.Reservations).ToList();
+            List<object> cReservation = new List<object>();
+            cReservation.Add(new[] { "Клієнт", "Кількість бронювань" });
+            foreach (var c in clients)
+            {
+                cReservation.Add(new object[] { c.FirstName + " " + c.LastName, c.Reservations.Count() });
+            }
+
+            return new JsonResult(cReservation);
+        }
+    }
+}
