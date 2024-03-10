84d54f0ea1f4fc741cbfd0a4dc7263e941c0fa9e
Veronika Deiko <deykonika2005@gmail.com>
2024-03-10 20:07:55 +0200

    added chart of typeServices

---
+++ BeautySalonInfrastructure/Controllers/ChartTypeServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautySalonInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartTypeServiceController : ControllerBase
+    {
+        private readonly DbbeautySalonContext _context;
+
+        public ChartTypeServiceController(DbbeautySalonContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]   
+        public JsonResult JsonData()
+        {
+            var typeServices = _context.TypeServices.Include(ts => ts.Services).ToList();
+            List<object> catService = new List<object>();
+            catService.Add(new[] { "Категорія", "Кількість послуг" });
+            foreach (var c in typeServices)
+            {
+                catService.Add(new object[] { c.Name, c.Services.Count() });
+            }
+
+            return new JsonResult(catService);
+        }
+
+
+    }
+
+
+}
